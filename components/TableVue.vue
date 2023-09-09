158223a13eca068fb9d1af8edcbabb69ce72b5cb
Jenny <jenny.maya186@gmail.com>
2023-09-09 13:15:27 +0700

    Updated

--- components/TableVue.vue
+++ components/TableVue.vue
@@ -1,100 +1,337 @@
 <template>
-  <div class="table-view">
-    <div class="table-title text-center text-2xl font-bold mb-5">
+  <div class="table-view bg-transparent-all">
+    <div class="table-title text-center text-3xl font-extrabold my-5">
       {{ title || "Welcome to UI Framework World" }}
     </div>
+
+    <div class="w-fit mb-3">
+      <USelectMenu
+        v-slot="{ open }"
+        v-model="selectedColumns"
+        :options="columns"
+        multiple
+        placeholder="Columns"
+        icon="material-symbols:check"
+        variant="outline"
+        value-attribute="name"
+        option-attribute="name"
+        :ui-menu="{ option: { base: 'cursor-pointer uppercase bg-transparent-all' } }"
+      >
+        <UButton color="transparent" variant="outline" class="px-4 ml-10">
+          {{
+            selectedColumns
+              .map((v) => {
+                return v.toUpperCase();
+              })
+              .join(" ,")
+          }}
+          <Icon
+            icon="material-symbols:arrow-right"
+            class="w-5 h-5 transition-transform"
+            :class="[open && 'transform rotate-90']"
+          />
+        </UButton>
+      </USelectMenu>
+    </div>
+    <div class="w-100 px-10 flex mb-3 bg-transparent-all justify-between">
+      <div class="flex bg-transparent-all">
+        <span> Rows Per Page </span>
+        <USelectMenu
+          v-model="pageCount"
+          :options="pageCountData"
+          placeholder="Per Page"
+          variant="outline"
+          value-attribute="value"
+          option-attribute="value"
+          icon="material-symbols:check"
+          :ui-menu="{ option: { base: 'cursor-pointer bg-transparent-all'} }"
+          :ui="{ option: { base: 'cursor-pointer bg-transparent-all'} }"
+        >
+          <UButton color="transparent" variant="outline" class="px-4 ml-5 bg-transparent-all">
+            {{ pageCount }}
+          </UButton>
+        </USelectMenu>
+      </div>
+      <UPagination
+        v-model="page"
+        :page-count="pageCount"
+        :total="filteredData.length"
+        :active-button="{ variant: 'outline' }"
+        :inactive-button="{ color: 'transparent', variant: 'outline' }"
+        class="bg-transparent-all"
+        :prev-button="{
+          label: 'Prev',
+          variant: 'outline',
+        }"
+        :next-button="{
+          trailing: true,
+          label: 'Next',
+          variant: 'outline',
+        }"
+      >
+        <template #prev="{ onClick }">
+          <UButton
+            color="transparent"
+            variant="outline"
+            class="rtl:[&_span:first-child]:rotate-180 me-2 bg-transparent-all"
+            @click="onClick"
+          >
+            <Icon
+              icon="material-symbols:arrow-left"
+              class="w-5 h-5 transition-transform bg-transparent-all"
+            />
+            Prev
+          </UButton>
+        </template>
+        <template #next="{ onClick }">
+          <UButton
+            color="transparent"
+            variant="outline"
+            class="rtl:[&_span:last-child]:rotate-180 ml-2-i bg-transparent-all"
+            @click="onClick"
+          >
+            Next
+            <Icon
+              icon="material-symbols:arrow-right"
+              class="w-5 h-5 transition-transform bg-transparent-all"
+            />
+          </UButton>
+        </template>
+      </UPagination>
+    </div>
+
+    <div class="ml-10 mb-3 bg-transparent-all">
+      <UInput
+        v-model="queryFilter"
+        class="w-1/4 bg-transparent-all p-2 bg-transparent-all"
+        placeholder="Filter ..."
+        :onchange="filteredRows"
+      />
+    </div>
+
     <div
-      class="px-10 py-3 flex justify-center items-center bg-transparent w-full gap-5 relative overflow-x-auto"
+      class="px-10 py-3 flex justify-center items-center bg-transparent-all w-full gap-5 relative overflow-x-auto"
     >
-      <table class="table w-full text-sm text-left" v-if="data?.length > 0">
+      <table class="table w-full text-sm text-left bg-transparent-all" v-if="data?.length > 0">
         <thead class="text-xs uppercase sticky top-0">
           <tr>
-            <td
-            scope="col"
-              :class="' text-center '+classString"
-              >
-              #
-            </td>
             <th
               scope="col"
-              :class="' text-center '+classString"
-              v-for="col in columns"
+              :class="' text-center font-extrabold text-base ' + classString"
             >
-              {{ col.name }}
+              #
             </th>
             <th
               scope="col"
-              :class="' text-center '+classString"
+              :class="' text-center font-extrabold text-base ' + classString"
+              v-for="(col, index) in selectedColumns"
+              :key="index"
             >
-              Actions
+              {{ col }}
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, index) in data">
-            <td :class="' text-center '+classString">
-              {{index + 1 }}
+          <tr v-for="(item, index) in selectedData" :key="index">
+            <td :class="' text-center ' + classString">
+              {{ index + 1 }}
             </td>
             <td
-              :class="col?.align ? ' text-'+col.align+' '+classString : classString"
-              v-for="col in columns"
+              :class="
+                col?.align
+                  ? ' text-' + col.align + ' ' + classString
+                  : classString
+              "
+              v-for="(col, index) in selectedColumns"
+              :key="index"
             >
-              {{ item[col.name] }}
-            </td>
-            <td
-              :class="classString"
-            >
-              <div class="flex justify-evenly">
-                <span
-                  class="bg-transparent border-2 px-3 py-1 border-white rounded-full cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300"
-                  >edit</span
-                >
-                <span
-                  class="bg-transparent border-2 px-3 py-1 border-white rounded-full cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300"
-                  >delete</span
-                >
-                <span
-                  class="bg-transparent border-2 px-3 py-1 border-white rounded-full cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300"
-                  >approve</span
-                >
-                <span
-                  class="bg-transparent border-2 px-3 py-1 border-white rounded-full cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300"
-                  >pending</span
-                >
-                <span
-                  class="bg-transparent border-2 px-3 py-1 border-white rounded-full cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300"
-                  >cancelled</span
-                >
-              </div>
+            <div v-if="isHTML(item[col.toLowerCase()])" v-html="item[col.toLowerCase()]"></div>
+            <div v-else>{{ item[col.toLowerCase()] }}</div>
             </td>
           </tr>
         </tbody>
       </table>
       <div v-else>No Data Available</div>
     </div>
+    <div class="w-100 px-10 flex mb-3 bg-transparent-all justify-between">
+      <div class="flex">
+        <span> Rows Per Page </span>
+        <USelectMenu
+          v-model="pageCount"
+          :options="pageCountData"
+          placeholder="Per Page"
+          variant="outline"
+          value-attribute="value"
+          option-attribute="value"
+          :ui-menu="{ option: { base: 'cursor-pointer bg-transparent-all' } }"
+        >
+          <UButton color="transparent" variant="outline" class="px-4 ml-5 bg-transparent-all">
+            {{ pageCount }}
+          </UButton>
+        </USelectMenu>
+      </div>
+      <UPagination
+        v-model="page"
+        :page-count="pageCount"
+        :total="filteredData.length"
+        :active-button="{ variant: 'outline' }"
+        :inactive-button="{ color: 'transparent', variant: 'outline' }"
+        :prev-button="{
+          label: 'Prev',
+          variant: 'outline',
+        }"
+        :next-button="{
+          trailing: true,
+          label: 'Next',
+          variant: 'outline',
+        }"
+        class="bg-transparent-all"
+      >
+        <template #prev="{ onClick }">
+          <UButton
+            color="transparent"
+            variant="outline"
+            class="rtl:[&_span:first-child]:rotate-180 me-2 bg-transparent-all"
+            @click="onClick"
+          >
+            <Icon
+              icon="material-symbols:arrow-left"
+              class="w-5 h-5 transition-transform bg-transparent-all"
+            />
+            Prev
+          </UButton>
+        </template>
+        <template #next="{ onClick }">
+          <UButton
+            color="transparent"
+            variant="outline"
+            class="rtl:[&_span:last-child]:rotate-180 ml-2-i bg-transparent-all"
+            @click="onClick"
+          >
+            Next
+            <Icon
+              icon="material-symbols:arrow-right"
+              class="w-5 h-5 transition-transform bg-transparent-all"
+            />
+          </UButton>
+        </template>
+      </UPagination>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { _1 } from "#tailwind-config/theme/aspectRatio";
+import { Icon } from "@iconify/vue";
 const props = defineProps({ data: Array, title: String, columns: Array });
 const data = ref(props.data);
+const filteredData = ref(props.data);
 const title = ref(props.title);
 const columns = ref(props.columns);
-const classString = ref("px-6 py-3 bg-transparent border-2 border-solid border-white")
+import Card from "./CardVue.vue"
+const page = ref(1);
+const pageCount = ref(5);
+const pageCountData = ref([
+  {
+    value: 5,
+  },
+  {
+    value: 10,
+  },
+  {
+    value: 20,
+  },
+  {
+    value: 30,
+  },
+  {
+    value: 50,
+  },
+  {
+    value: 100,
+  },
+]);
+const selectedColumns = ref([]);
+const selectedData = ref([]);
+const classString = ref(
+  "px-6 py-2 bg-transparent-all border-2 border-solid border-white"
+);
+
+const queryFilter = ref("");
+
+
 onMounted(() => {
   if (!columns.value)
     if (data?.value?.length > 0) {
-      columns.value = getColumnName(Object.keys(data.value[0]));
+      columns.value = getColumnNameObject(Object.keys(data.value[0]));
     }
+
+  selectedColumns.value = getColumnArray(columns.value);
+  selectedData.value = data?.value?.slice(0, pageCount.value);
+
+  console.log("selectedData: onMounted ", pageCount.value);
 });
 
-const getColumnName = (columnsName) => {
+watch(queryFilter, (newData) => {
+  console.log("selectedData: ", selectedData.value);
+  console.log("queryFilter is " + queryFilter + " now " + newData);
+});
+
+watch(page, (newData) => {
+  let from = 0;
+  let to = pageCount.value;
+
+  if (page.value > 1) {
+    to = page.value * pageCount.value;
+    from = to - pageCount.value;
+  }
+  selectedData.value = filteredData.value.slice(from, to);
+});
+
+watch(pageCount, (newData) => {
+  selectedData.value = filteredData.value.slice(0, newData);
+});
+
+const filteredRows = computed(() => {
+  console.log("fileter called");
+  if (!queryFilter.value) {
+    selectedData.value = data?.value;
+    filteredData.value = data?.value;
+    return data;
+  }
+  const res = data.value.filter((user) => {
+    return Object.values(user).some((value) => {
+      return String(value)
+        .toLowerCase()
+        .includes(queryFilter.value.toLowerCase());
+    });
+  });
+  filteredData.value = res;
+  selectedData.value = res.slice(0, pageCount.value);
+  console.log("res length ", res.length);
+  return res;
+});
+
+const getColumnArray = (col) => {
+  const res = [];
+  col.map((c) => {
+    res.push(c.name);
+  });
+  return res;
+};
+
+const getColumnNameObject = (columnsName) => {
   const data = [];
   columnsName.map((n) => {
     data.push({
       name: n,
     });
   });
-  return data
+  return data;
 };
+
+const isHTML = (str) => {
+      const doc = new DOMParser().parseFromString(str, "text/html");
+      return Array.from(doc.body.childNodes).some(node => node.nodeType === 1); // Node type 1 is an element node
+    }
 </script>
--- pages/settings/users/index.vue
+++ pages/settings/users/index.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
-    <div>
-      <TableVue title="Users Management" :data="people" :columns="columns"/>
+    <div class="bg-transparent-all">
+      <TableVue title="Users Management" :data="people" :columns="columns" />
     </div>
   </NuxtLayout>
 </template>
@@ -9,177 +9,333 @@
 <script setup>
 import TableVue from "~/components/TableVue.vue";
 
-const people = [{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member',
-  salary: '5000'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-},
-{
-  id: 7,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 8,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 9,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 12,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-},
-{
-  id: 1,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 2,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 3,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 4,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 5,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 6,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-},
-{
-  id: 7,
-  name: 'Lindsay Walton',
-  title: 'Front-end Developer',
-  email: 'lindsay.walton@example.com',
-  role: 'Member'
-}, {
-  id: 8,
-  name: 'Courtney Henry',
-  title: 'Designer',
-  email: 'courtney.henry@example.com',
-  role: 'Admin'
-}, {
-  id: 9,
-  name: 'Tom Cook',
-  title: 'Director of Product',
-  email: 'tom.cook@example.com',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'Whitney Francis',
-  title: 'Copywriter',
-  email: 'whitney.francis@example.com',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Leonard Krasner',
-  title: 'Senior Designer',
-  email: 'leonard.krasner@example.com',
-  role: 'Owner'
-}, {
-  id: 12,
-  name: 'Floyd Miles',
-  title: 'Principal Designer',
-  email: 'floyd.miles@example.com',
-  role: 'Member'
-}
-]
+const actionCssClass =
+  "bg-transparent-all border-2 px-3 py-1 border-white rounded-full uppercase cursor-pointer hover:border-10 hover:border-purple-300 hover:transition hover:ease-in-out focus:border-purple-600 hover:text-purple-300";
+
+const action = `<div class="flex bg-transparent-all justify-evenly">
+                <span class="${actionCssClass}">edit</span>
+                <span class="${actionCssClass}">delete</span>
+                <span class="${actionCssClass}">approve</span>
+                <span class="${actionCssClass}">reject</span>
+                <span class="${actionCssClass}">cancelled</span>
+              </div>`;
+
+const people = [
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+    salary: "5000",
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 7,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 8,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 9,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 10,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 11,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 12,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 7,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 8,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 9,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 10,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 11,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 12,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 1,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 2,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 3,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 4,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 5,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 6,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 7,
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 8,
+    name: "Courtney Henry",
+    title: "Designer",
+    email: "courtney.henry@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 9,
+    name: "Tom Cook",
+    title: "Director of Product",
+    email: "tom.cook@example.com",
+    role: "Member",
+    action: action,
+  },
+  {
+    id: 10,
+    name: "Whitney Francis",
+    title: "Copywriter",
+    email: "whitney.francis@example.com",
+    role: "Admin",
+    action: action,
+  },
+  {
+    id: 11,
+    name: "Leonard Krasner",
+    title: "Senior Designer",
+    email: "leonard.krasner@example.com",
+    role: "Owner",
+    action: action,
+  },
+  {
+    id: 12,
+    name: "Floyd Miles",
+    title: "Principal Designer",
+    email: "floyd.miles@example.com",
+    role: "Member",
+    action: action,
+  },
+];
 
 const columns = [
-    /* {
+  /* {
         "name": "id",
         "align": "center",
     }, */
-    {
-        "name": "name",
-        "align": "left",
-    },
-    {
-        "name": "title",
-        "align": "right",
-    },
-    {
-        "name": "email",
-        "align": "center",
-    },
-    {
-        "name": "role",
-        "align": "center",
-    }
-]
+  {
+    name: "name",
+    align: "left",
+  },
+  {
+    name: "title",
+    align: "right",
+  },
+  {
+    name: "email",
+    align: "center",
+  },
+  {
+    name: "role",
+    align: "center",
+  },
+  {
+    name: "action",
+    align: "center",
+  },
+];
 </script>
